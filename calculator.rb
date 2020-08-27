fa91b1e7d6b60fe07997bbbd6c9badec916b8774
Rodrigo Rojas <carlorrojas@gmail.com>
2020-08-27 05:37:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
-first_number = 7
+first_number = 7.0
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
